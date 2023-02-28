3e65e0a7a7ad53ffc10889a87e8f688832e4feff
Camille Jaumier <camille.jaumier@icloud.com>
2023-02-28 11:57:42 +0100

    creation seed

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
 class Booking < ApplicationRecord
   belongs_to :castle
   belongs_to :user
+  validates :total_price, presence: true, numericality: { only_integer: true }
+  validates :validate, presence: true
+  validates :check_in, numericality: { only_date: true }
+  validates :check_out, numericality: { only_date: true }
+  validates :check_in, comparison: { greater_than: :check_out }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   has_many :bookings
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :description, presence: true
+  validates :photo, presence: true
 end
