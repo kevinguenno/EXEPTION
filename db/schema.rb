4f95b7a5dff29ca7f5bcdff24c688162aabba6e5
Camille Jaumier <camille.jaumier@icloud.com>
2023-02-28 11:45:20 +0100

    migration users

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_164002) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_104412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "check_in"
+    t.date "check_out"
+    t.string "validates"
+    t.integer "total_price"
+    t.bigint "castle_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["castle_id"], name: "index_bookings_on_castle_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "castles", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "location"
+    t.integer "price"
+    t.integer "capacity"
+    t.string "photo"
+    t.string "category"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_castles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +49,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_164002) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.text "description"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "castles"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "castles", "users"
 end
