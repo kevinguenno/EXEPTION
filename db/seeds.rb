8f9db8222b159c2967bc877380e9e7d24f3cec20
Camille Jaumier <camille.jaumier@icloud.com>
2023-02-28 16:15:18 +0100

    seed finish & cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,14 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+puts "Cleaning the database"
+
 Booking.destroy_all
 Castle.destroy_all
 User.destroy_all
+puts "Database CLEAN"
 
+puts "Creating owner"
 owner1 = User.create!({
   email: "charles.richelieu@icloud.com",
   password: "1234567890",
@@ -18,18 +22,21 @@ owner1 = User.create!({
   description: "Hello, I'm Charles Richelieu
   I am passionate about old architecture and would be delighted to show you my home, which is rich in history.
   You will have a pleasant stay with your family and friends. The estate can also be booked for ceremonies",
-  photo: "photo"
 })
+file = File.open(Rails.root.join("db/seeds/images/users/homme1.jpg"))
+owner1.photo.attach(io: file, filename: "homme1.jpg", content_type: "image/jpeg")
+
 castle1 = Castle.new({
   title: "Incredible Castle",
   description: "Incredible French Castle",
   location: "Tours",
   price: 1435,
   capacity: 25,
-  photo: "photo",
   category: "Countryside"
 })
 castle1.user = owner1
+file = File.open(Rails.root.join("db/seeds/images/castles/chateau1.jpg"))
+castle1.photo.attach(io: file, filename: "chateaucountryside.jpg", content_type: "image/jpeg")
 castle1.save!
 
 #############
@@ -42,18 +49,21 @@ owner2 = User.create!({
   description: "Hello, I'm Madeleine Sterma
   I rent out several of my homes to offer you unique moments with family or friends,
   or to make your birthdays, weddings and all your ceremonies unforgettable.",
-  photo: "photo"
 })
+file = File.open(Rails.root.join("db/seeds/images/users/femme1.jpg"))
+owner2.photo.attach(io: file, filename: "femme1.jpg", content_type: "image/jpeg")
+
 castle2 = Castle.new({
   title: "Small country castle",
   description: "Small castle in the Bordeaux countryside with swimming pool and breathtaking views of the vineyards",
   location: "Bordeaux",
   price: 875,
   capacity: 12,
-  photo: "photo",
   category: "Countryside"
 })
 castle2.user = owner2
+file = File.open(Rails.root.join("db/seeds/images/castles/chateau2.jpg"))
+castle2.photo.attach(io: file, filename: "chateaucountryside.jpg", content_type: "image/jpeg")
 castle2.save!
 
 #############
@@ -65,8 +75,10 @@ owner3 = User.create!({
   last_name: "Rabiller",
   description: "Hello, I'm Georges Rabiller
   Come and discover my castles in the south of France to spend your holidays or to organise your events.",
-  photo: "photo"
 })
+file = File.open(Rails.root.join("db/seeds/images/users/homme2.jpg"))
+owner3.photo.attach(io: file, filename: "homme2.jpg", content_type: "image/jpeg")
+
 castle3 = Castle.new({
   title: "The sunny castle",
   description: "French Riviera castle to offer you warmth and rest not far from St-Tropez (20min)
@@ -74,10 +86,11 @@ castle3 = Castle.new({
   location: "St-Tropez",
   price: 2220,
   capacity: 20,
-  photo: "photo",
   category: "Sea and beach"
 })
 castle3.user = owner3
+file = File.open(Rails.root.join("db/seeds/images/castles/chateau3.jpg"))
+castle3.photo.attach(io: file, filename: "chateauseaandbeach.jpg", content_type: "image/jpeg")
 castle3.save!
 
 #############
@@ -88,8 +101,10 @@ owner4 = User.create!({
   last_name: "Bridton",
   description: "Hello, I'm Grace Bridton
   Would you like to discover the Basque coast through an unusual rental experience? Come and stay in one of my homes on the West Coast.",
-  photo: "photo"
 })
+file = File.open(Rails.root.join("db/seeds/images/users/femme2.jpg"))
+owner4.photo.attach(io: file, filename: "femme2.jpg", content_type: "image/jpeg")
+
 castle4 = Castle.new({
   title: "The Basque castle",
   description: "Enjoy the adventure of the Basque coast by staying in a castle full of history and life.
@@ -97,10 +112,11 @@ castle4 = Castle.new({
   location: "St-Jean-de-Luz",
   price: 1150,
   capacity: 15,
-  photo: "photo",
   category: "Sea and beach"
 })
 castle4.user = owner4
+file = File.open(Rails.root.join("db/seeds/images/castles/chateau4.jpg"))
+castle4.photo.attach(io: file, filename: "chateauseaandbeach.jpg", content_type: "image/jpeg")
 castle4.save!
 
 #############
@@ -112,8 +128,10 @@ owner5 = User.create!({
   description: "Hello, I'm Pierre Legrand
   I would be delighted to welcome you to one of my castles in Brittany.
   Let yourself be guided by the sea spray and the smell of pancakes, and treat yourself to a dream holiday.",
-  photo: "photo"
 })
+file = File.open(Rails.root.join("db/seeds/images/users/homme3.jpg"))
+owner5.photo.attach(io: file, filename: "homme3.jpg", content_type: "image/jpeg")
+
 castle5 = Castle.new({
   title: "The Breton Castle",
   description: "Large estate, heated and covered swimming pool and barbecue are at your disposal.
@@ -121,10 +139,11 @@ castle5 = Castle.new({
   location: "Belle-île-en-Mer",
   price: 925,
   capacity: 10,
-  photo: "photo",
   category: "Sea and beach"
 })
 castle5.user = owner5
+file = File.open(Rails.root.join("db/seeds/images/castles/chateau5.jpg"))
+castle5.photo.attach(io: file, filename: "chateauseaandbeach.jpg", content_type: "image/jpeg")
 castle5.save!
 
 #############
@@ -137,8 +156,10 @@ owner6 = User.create!({
   Looking for a relaxing holiday away from the hustle and bustle of the big cities?
   My castles in the centre of France will be perfect for your holidays with family or friends.
   The castles are suitable for everyone, young and old, grandma and grandpa. ",
-  photo: "photo"
 })
+file = File.open(Rails.root.join("db/seeds/images/users/femme3.jpeg"))
+owner6.photo.attach(io: file, filename: "femme3.jpg", content_type: "image/jpeg")
+
 castle6 = Castle.new({
   title: "Auvergne Castle",
   description: "The castle has a heated and covered swimming pool.
@@ -147,22 +168,29 @@ castle6 = Castle.new({
   location: "Puy-de-dôme",
   price: 915,
   capacity: 12,
-  photo: "photo",
   category: "Countryside"
 })
 castle6.user = owner6
+file = File.open(Rails.root.join("db/seeds/images/castles/chateau6.jpg"))
+castle6.photo.attach(io: file, filename: "chateaucountryside.jpg", content_type: "image/jpeg")
 castle6.save!
 
+puts "Owner created"
+
 ##################################
+puts "Creating renter"
+
 renter1 = User.create!({
   email: "laurent.bourgeois@gmail.com",
   password: "1234567890",
   first_name: "Laurent",
   last_name: "Bourgeois",
   description: "Hello, I'm Laurent Bourgeois.
-  I enjoy discovering places full of history and sharing moments with my family and friends.",
-  photo: "photo"
+  I enjoy discovering places full of history and sharing moments with my family and friends."
 })
+file = File.open(Rails.root.join("db/seeds/images/users/homme4.jpg"))
+renter1.photo.attach(io: file, filename: "homme4.jpg", content_type: "image/jpeg")
+
 booking1 = Booking.new({
   check_in: Date.new(2023, 03, 01),
   check_out: Date.new(2023, 03, 15),
@@ -181,8 +209,10 @@ renter2 = User.create!({
   last_name: "Maunier",
   description: "Hello, I'm Brice Maunier.
   I like sports and beautiful places.",
-  photo: "photo"
-  })
+})
+file = File.open(Rails.root.join("db/seeds/images/users/homme5.jpg"))
+renter2.photo.attach(io: file, filename: "homme5.jpg", content_type: "image/jpeg")
+
 booking2 = Booking.new({
   check_in: Date.new(2023, 06, 02),
   check_out: Date.new(2023, 06, 15),
@@ -201,11 +231,13 @@ renter3 = User.create!({
   last_name: "Lemaire",
   description: "Hello, I'm Claire Lemaire.
   I want to discover the most beautiful buildings in France to spend my holidays with my family.",
-  photo: "photo"
 })
+file = File.open(Rails.root.join("db/seeds/images/users/femme4.jpg"))
+renter3.photo.attach(io: file, filename: "femme4.jpg", content_type: "image/jpeg")
+
 booking3 = Booking.new({
-  check_in: Date.new(2023, 08, 01),
-  check_out: Date.new(2023, 08, 07),
+  check_in: Date.new(2023, 8, 01),
+  check_out: Date.new(2023, 8, 07),
   total_price: 4675,
   validates: "Pending"
 })
@@ -222,8 +254,10 @@ renter4 = User.create!({
   last_name: "Guerin",
   description: "Hello, I'm Laetitia Guerin.
   I am a wedding planner and I am looking for exceptional places for ceremonies (baptism, wedding, birthday, birth...)",
-  photo: "photo"
-  })
+})
+file = File.open(Rails.root.join("db/seeds/images/users/femme5.jpg"))
+renter4.photo.attach(io: file, filename: "femme5.jpg", content_type: "image/jpeg")
+
 booking4 = Booking.new({
   check_in: Date.new(2023, 05, 02),
   check_out: Date.new(2023, 05, 04),
@@ -242,12 +276,14 @@ renter5 = User.create!({
   first_name: "Maxime",
   last_name: "Bardin",
   description: "Hello, I'm Maxime Bardin.
-  I like to share holidays with my large family in exceptional places.",
-  photo: "photo"
-  })
+  I like to share holidays with my large family in exceptional places."
+})
+file = File.open(Rails.root.join("db/seeds/images/users/homme6.jpg"))
+renter5.photo.attach(io: file, filename: "homme6.jpg", content_type: "image/jpeg")
+
 booking5 = Booking.new({
-  check_in: Date.new(2023, 09, 12),
-  check_out: Date.new(2023, 09, 20),
+  check_in: Date.new(2023, 9, 12),
+  check_out: Date.new(2023, 9, 20),
   total_price: 6275,
   validates: "Pending"
 })
@@ -264,9 +300,11 @@ renter6 = User.create!({
   first_name: "Stéphanie",
   last_name: "Legreat",
   description: "Hello, I'm Stéphanie Legreat.
-  I am looking for exceptional holiday locations where young and old can have fun.",
-  photo: "photo"
-  })
+  I am looking for exceptional holiday locations where young and old can have fun."
+})
+file = File.open(Rails.root.join("db/seeds/images/users/femme6.jpg"))
+renter6.photo.attach(io: file, filename: "femme6.jpg", content_type: "image/jpeg")
+
 booking6 = Booking.new({
   check_in: Date.new(2023, 07, 01),
   check_out: Date.new(2023, 07, 15),
@@ -276,3 +314,5 @@ booking6 = Booking.new({
 booking6.user = renter6
 booking6.castle = castle6
 booking6.save!
+
+puts "Renter created"
