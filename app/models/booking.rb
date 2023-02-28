769af1fe0f5f342f75c5cc65fafd9b734ec67567
Camille Jaumier <camille.jaumier@icloud.com>
2023-02-28 12:24:33 +0100

    seeds user, castle, booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,8 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :castle
   belongs_to :user
   validates :total_price, presence: true, numericality: { only_integer: true }
-  validates :validate, presence: true
-  validates :check_in, numericality: { only_date: true }
-  validates :check_out, numericality: { only_date: true }
-  validates :check_in, comparison: { greater_than: :check_out }
+  validates :validates, presence: true
+  validates :check_in, presence: true
+  validates :check_out, presence: true
+  validates :check_out, comparison: { greater_than: :check_in }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,17 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create!({ email: "example@gmail.com", password: "lpb", first_name: "Charles", last_name: "Richelieu", description: "Hello, I'm Charles Richelieu", photo: "photo" })
+Booking.destroy_all
+Castle.destroy_all
+User.destroy_all
+
+user1 = User.create!({ email: "example@gmail.com", password: "leplusbeaudetous", first_name: "Charles", last_name: "Richelieu", description: "Hello, I'm Charles Richelieu", photo: "photo" })
+castle1 = Castle.new({ title: "Incredible Castle", description: "Incredible Castle in France", location: "Tours", price: 875, capacity: 15, photo: "photo", category: "French Castle"})
+castle1.user = user1
+castle1.save!
+
+user2 = User.create!({ email: "kevin@gmail.com", password: "kevinleplusbeaudetous", first_name: "Kevin", last_name: "Guenno", description: "Hello, I'm Kevin Guenno", photo: "photo" })
+booking1 = Booking.new({ check_in: Date.new(2023,03,01), check_out: Date.new(2023,03,15), total_price: 6525, validates: "Pending" })
+booking1.user = user2
+booking1.castle = castle1
+booking1.save!
